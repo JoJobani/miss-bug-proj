9711f8fe29c9a1a55ca18f7b033207db3eeb0540
JoJobani <hvu2309@gmail.com>
2024-07-03 15:40:06 +0300

    Implement bug ownership

--- public/cmps/BugList.jsx
+++ public/cmps/BugList.jsx
@@ -1,18 +1,31 @@
 const { Link } = ReactRouterDOM
 
 import { BugPreview } from './BugPreview.jsx'
+import { userService } from '../services/user.service.js'
 
 export function BugList({ bugs, onRemoveBug, onEditBug }) {
+
+    const user = userService.getLoggedUser()
+
+    function isOwner(bug) {
+        if (!user) return false
+        if (!bug.creator) return true
+        return user.isAdmin || bug.creator._id === user._id
+    }
+
     return (
         <ul className="bug-list">
             {bugs.map((bug) => (
                 <li className="bug-preview" key={bug._id}>
                     <BugPreview bug={bug} />
-                    <section className='bug-controls'>
-                        <button onClick={() => onRemoveBug(bug._id)}>x</button>
-                        <button onClick={() => onEditBug(bug)}>Edit</button>
-                        <Link to={`/bug/${bug._id}`}>Details</Link>
-                    </section>
+                    <Link to={`/bug/${bug._id}`}>Details</Link>
+                    {
+                        isOwner(bug) &&
+                        <section className='bug-controls'>
+                            <button onClick={() => onRemoveBug(bug._id)}>x</button>
+                            <button onClick={() => onEditBug(bug)}>Edit</button>
+                        </section>
+                    }
                 </li>
             ))
             }
